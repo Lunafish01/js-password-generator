47b575d07ce03f58da7048ca8fe84523e32c8de8
Lunafish01 <142092354+Lunafish01@users.noreply.github.com>
2023-09-11 15:14:29 -0500

    updated script.js file

--- script.js
+++ script.js
@@ -1,45 +1,46 @@
-// Assignment Code
+  // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// declared empty random passowrd array
+  // declared empty random passowrd array
 var randomPassword = []
+var characterLength = 8
 
-//declared character variables 
+  //declared character variables 
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var specialChar = ["!","@","#","$","%","^","&","*","(",")",];
 var numbers = ["0","1","2","3","4","5","6","7","8","9",];
 
-// Write password to the #password input
+  // Write password to the #password input
 function writePassword() {
+  // calling new function to begin user prompt
+  var correctPrompt = userPrompt(); 
+
+if (correctPrompt)
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+  passwordText.value = password;  
 }
+  // 1. Prompt user for password criteria
+  //    a. password length 8 < 128
+  //    b. Uppercase
+  //    c. Lowercase
+  //    d. Numbers
+  //    e. Special Characters
+  //prompt the user for password criteria. Asking for password length and what kind of characters they want to include 
+function userPrompt() {
+  randomPassword = []
 
-function generatePassword() {
-  console.log("Hey! You clicked the button!")
-  // 4. Display password on the page
-  return "Generated password will go here!"
-}
-
-// 1. Prompt user for password criteria
-//    a. password length 8 < 128
-//    b. Uppercase
-//    c. Lowercase
-//    d. Numbers
-//    e. Special Characters
-
-//This fuction will prompt the user for password criteria. Asking for password length and what kind of characters they want to include 
-function getPrompt() {
   characterLength = parseInt(prompt("How long do you want your password to be? between 8 - 128 characters."));
 
-  if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
-    alert("Character lenght has to be a number between 8 - 128 digits. Please try again.");
+  // 2. Validate user input 
+  //validating the character length and value of the user password input to make sure they enter allowed length and value type
+  //validating user enters number and not a string
+  if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+    alert("Character lenght must be a number between 8 - 128 digits.");
     return false;
   }
-
+  //if true, prompt user for characters to include in password
   if (confirm("Do you want to include uppercase letters in your password?")) {
     randomPassword = randomPassword.concat(upperCase);
   }
@@ -52,14 +53,21 @@ function getPrompt() {
   if (confirm("Do you want to include numbers in your password?")) {
     randomPassword = randomPassword.concat(numbers);
   }
+  return true;
 }
-// 2. Validate user input 
-// 3. Generate random password
 
-
-
-
-
+  // 3. Generate random password
+function generatePassword() {
+  //use correct prompt criteria to generate randomPassword
+  var newPassword = " ";
+  //for loop to start generating random Math method to call random values from variable arrays
+  for(var i = 0; i < characterLength; i++) {
+    var randomValue = Math.floor(Math.random() * randomPassword.length); 
+  newPassword = newPassword + randomPassword[randomValue];
+  }
+  return newPassword;
+}  
+  
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
