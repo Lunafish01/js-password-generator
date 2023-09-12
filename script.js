ff246efa761d7fe714245dcc18467736a6181a27
Lunafish01 <142092354+Lunafish01@users.noreply.github.com>
2023-09-11 23:23:26 -0500

    final update to script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
   // Assignment Code
 var generateBtn = document.querySelector("#generate");
   // declared empty random passowrd array and character arrays
-var randomPassword = []
 var characterLength = 8
+var randomPassword = []
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var specialChar = ["!","@","#","$","%","^","&","*","(",")",];
